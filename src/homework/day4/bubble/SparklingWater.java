efacc45f99bf41cefbc1abb4933ce668234b0382
shkadar79 <shkadar79@gmail.com>
2022-10-15 02:14:05 +0300

    Bubble.homework is in process...

---
+++ src/homework/day4/bubble/SparklingWater.java
@@ -0,0 +1,14 @@
+package homework.day4.bubble;
+
+public class SparklingWater extends Water {
+    private boolean isOpened;
+    private Bubble[] bubbles;
+
+    public SparklingWater(boolean isOpened, Bubble[] bubbles) {
+        this.isOpened = isOpened;
+        this.bubbles = bubbles;
+    }
+     @Override public void pump(Bubble[] bubbles) {
+
+     }
+}
