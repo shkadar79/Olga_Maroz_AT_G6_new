c322f099d508fcc60f1c5aab7cddd56295168ff4
shkadar79 <shkadar79@gmail.com>
2022-10-15 01:40:59 +0300

    I've finished day4.array.homework.

---
+++ src/homework/day4/array/ArrayTask3.java
@@ -0,0 +1,26 @@
+package homework.day4.array;
+
+import java.util.Random;
+
+public class ArrayTask3 {
+    public void arraySumDevisibleByTen(int[] myArray) {
+        int sum = 0;
+        for (int a : myArray) {
+            if (a % 10 == 0) {
+                sum += a;
+            }
+        }
+        System.out.println(sum);
+    }
+
+    public int[] getArray(int n) {
+        Random random = new Random();
+        int[] myArray = new int[n];
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = random.nextInt(100);
+        }
+        return myArray;
+    }
+
+}
+
