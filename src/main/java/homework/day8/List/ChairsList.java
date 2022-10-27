a131f4066d94d090e203e97df48228035109e7ef
shkadar79 <shkadar79@gmail.com>
2022-10-27 22:23:13 +0300

    I've modified Task15 (Furniture).

--- src/main/java/homework/day8/List/ChairsList.java
+++ src/main/java/homework/day8/List/ChairsList.java
@@ -7,23 +7,23 @@ import java.util.*;
 public class ChairsList {
     public static void main(String[] args) {
         List<Chair> furniture = new ArrayList<>(Arrays.asList(new Chair(1, 2), new Chair(4, 2), new Chair(2, 4)));
-        int counter = 1;
+
         for (Chair i : furniture) {
-            counter = i.getHeight() * i.getWidth();
-            System.out.print(counter + " ");
+            System.out.print((i.getHeight() * i.getWidth()) + " ");
         }
         System.out.println();
+
         Map<Integer, Chair> map = new HashMap<>();
-        for (int i = 0; i< furniture.size(); i++){
+        for (int i = 0; i < furniture.size(); i++) {
             map.put(i, furniture.get(i));
         }
-        for (int i : map.keySet()){
+        for (int i : map.keySet()) {
             System.out.println(i);
         }
-        for (Chair i : map.values()){
+        for (Chair i : map.values()) {
             System.out.println(i);
         }
-        for (Map.Entry<Integer, Chair> combo : map.entrySet()){
+        for (Map.Entry<Integer, Chair> combo : map.entrySet()) {
             System.out.println(combo);
         }
     }
